5dacd1c23b01eab9608af08bf0a4f36a298c8cff
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-23 02:58:18 +0800

    ✨ navbar route

---
+++ backend/routes/Navbar.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router()
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+    try {
+        const supportedLanguages = ['en', 'fr'];
+        let lang = req.query.lang;
+
+        if (!lang) {
+            lang = 'en';
+        }
+
+        if (!supportedLanguages.includes(lang)) {
+            return res.status(400).json({ error: 'Unsupported language' });
+        }
+
+        const navbar = await prisma.navbar.findMany({
+            include: {
+                translations: {
+                    where: {
+                        translationCode: lang
+                    }
+                }
+            }
+        });
+
+        const filteredNavbar = navbar.filter(navItem => navItem.translations.length > 0);
+
+        res.json({ languages: filteredNavbar });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Error while getting languages' });
+    }
+});
+
+module.exports = router
