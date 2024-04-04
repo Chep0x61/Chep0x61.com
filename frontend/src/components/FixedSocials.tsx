1166dc4151b22269575a4c6c7ba3b75c9d141809
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-04-04 20:48:36 +0800

    ✨ fixed socials component

--- frontend/src/components/FixedSocials.tsx
+++ frontend/src/components/FixedSocials.tsx
@@ -1,12 +1,16 @@
+import Mail from "@/components/icons/Mail";
+import LinkedIn from "@/components/icons/LinkedIn";
+import Github from "@/components/icons/Github";
+
 const FixedSocials = () => {
     return (
       <div className="hidden lg:flex fixed flex-col items-center bottom-0 left-12">
-      <div className="flex flex-col gap-6 items-center pb-2">
-      <div>GITHUB</div>
-      <div>LINKEDIN</div>
-      <div>MAIL</div>
-      </div>
-      <div className="border-r-2 border-gray-500 h-16"></div>
+          <div className="flex flex-col gap-6 items-center pb-6">
+              <Github />
+              <Mail />
+              <LinkedIn />
+          </div>
+          <div className="border-r-2 border-[#1a1a1a] dark:border-[#f8f8f8] h-24"></div>
       </div>
     )
 }
